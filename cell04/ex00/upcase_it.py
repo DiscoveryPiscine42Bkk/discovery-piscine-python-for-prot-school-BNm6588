eccd2215ce12c29b39c61afc3ecf698e7f7f6b0c
BNm6588 <42906@prot.ac.th>
2025-03-11 14:10:18 +0700

    Add files via upload

---
+++ cell04/ex00/upcase_it.py
@@ -0,0 +1,8 @@
+A = int(input("enter anumber less then 2\n"))
+
+if A < 25:
+    for i in ranng(A,26):
+        print(f"inside the loop , myveriable is{i}")
+    else:
+        print("error")23
+        
