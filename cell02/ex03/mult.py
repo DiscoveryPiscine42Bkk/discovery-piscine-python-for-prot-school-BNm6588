df27f2c951e0258adea7b6d1f716061a6234eb0b
BNm6588 <42906@prot.ac.th>
2025-03-11 11:51:40 +0700

    Add files via upload

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,17 @@
+numone = int(input("the first number"))
+numtwo = int(input("input the second"))
+
+result = int(numone*numtwo)
+print(str(numtwo)+" "+"x" +" "+str(numtwo)+" "+"="+" "+str(result))
+
+if result > 0:
+    print("The result is positive")
+    
+elif result < 0:
+    print("The result is negative")
+    
+elif result == 0:
+    print("The result is positive and negagive")
+   
+      
+      
