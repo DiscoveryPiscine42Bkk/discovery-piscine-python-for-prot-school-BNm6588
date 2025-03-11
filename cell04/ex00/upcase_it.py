280324f682011631fc2b30ea4acc680d33723edb
BNm6588 <42906@prot.ac.th>
2025-03-11 14:14:03 +0700

    Delete cell04/ex00/upcase_it.py

--- cell04/ex00/upcase_it.py
+++
@@ -1,8 +0,0 @@
-A = int(input("enter anumber less then 2\n"))
-
-if A < 25:
-    for i in ranng(A,26):
-        print(f"inside the loop , myveriable is{i}")
-    else:
-        print("error")23
-        
