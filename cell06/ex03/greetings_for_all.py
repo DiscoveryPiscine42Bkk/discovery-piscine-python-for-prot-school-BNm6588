74132c4df51231f80ceb1a3a6674f199405aa36b
BNm6588 <42906@prot.ac.th>
2025-03-12 13:54:03 +0700

    Add files via upload

---
+++ cell06/ex03/greetings_for_all.py
@@ -0,0 +1,9 @@
+def greetings(name="noble stranger"):
+    if not isinstance(name, str):
+        print("Error: Name must be a string. ")
+    else:
+        print(f"welcom, {name}!")
+    
+        greetings()
+        greetings("John")
+        greetings(123)
