c0e84c56413a6ce880bf57cf55f5b2a7e4637a71
BNm6588 <42906@prot.ac.th>
2025-03-12 09:25:45 +0700

    Delete cell04/ex05/up_low.py

--- cell04/ex05/up_low.py
+++
@@ -1,6 +0,0 @@
-def swap_case(input_string):
-    return input_string.swapcase()
-
-if __name__ == "__main__":
-    user_input = input("Enter a string: ")
-    print("Modified string:", swap_case(user_input))
