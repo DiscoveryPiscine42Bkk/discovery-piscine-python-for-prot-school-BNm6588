ff775365cf4007ef29aced5b105a3e155333dec4
BNm6588 <42906@prot.ac.th>
2025-03-12 11:00:16 +0700

    Delete cell05/ex0000 directory

--- cell05/ex0000/play_with_arrays.py
+++
@@ -1,8 +0,0 @@
-def create_2d_array():
-    arr_2d = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-    return arr_2d
-if __name__ == "__main__":
-    array = create_2d_array()
-    print("Created 2D Array:")
-    for row in array:
-        print(row)
