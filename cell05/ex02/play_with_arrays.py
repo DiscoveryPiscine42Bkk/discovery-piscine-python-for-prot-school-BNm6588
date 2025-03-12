fdb5f6bac04a5c6e2089abe1b82e45da2de87c39
BNm6588 <42906@prot.ac.th>
2025-03-12 11:30:54 +0700

    Add files via upload

---
+++ cell05/ex02/play_with_arrays.py
@@ -0,0 +1,9 @@
+def create_2d_array():
+    arr_2d = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    return arr_2d
+if __name__ == "__main__":
+    array = create_2d_array()
+    print("Created 2D Array:")
+    for row in array:[2, 8, 9, 48, 8, 22, -12, 2]
+[10, 11, 50, 10, 24]
+print(row)
